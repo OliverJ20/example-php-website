863cbc619fbbec5d17eae8c875544ced3b5fad91
OliverJ20 <oj2008@live.com.au>
2018-02-04 13:23:00 +1000

    wesbite example files

---
+++ globe_bank/public/staff/pages/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>Globe Bank</title>
+    <meta charset="utf-8">
+  </head>
+
+  <body>
+  <h1> Globe bank : comming soon</h1>
+
+  </body>
+</html>
